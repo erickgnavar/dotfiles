1ef5dd002dd2735876c5447ad6bafd9982a830a9
Erick Navarro <erick@navarro.io>
2019-06-08 11:50:52 -0700

    [zsh] Remove iterm2 shell integration
    I replaced iTerm2 with Alacritty

--- zsh/mac.sh
+++ zsh/mac.sh
@@ -5,6 +5,3 @@ export PATH=/usr/local/bin:$PATH:/Applications/Postgres.app/Contents/Versions/la
 export WORKON_HOME=~/.virtualenvs
 source ~/Library/Python/2.7/bin/virtualenvwrapper.sh
 # TODO: change to relative path
-
-# iterm2 integration
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"

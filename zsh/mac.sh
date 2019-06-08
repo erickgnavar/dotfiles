b1c2e9a5150e30ee89588b833d5e6052970bd748
Erick Navarro <erick@navarro.io>
2019-06-08 13:14:18 -0700

    [zsh] Setup gettext location in path for macOS

--- zsh/mac.sh
+++ zsh/mac.sh
@@ -1,6 +1,9 @@
 # postgres.app setup
 export PATH=/usr/local/bin:$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 
+# Setup gettext binaries
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+
 # virtualenvwrapper
 export WORKON_HOME=~/.virtualenvs
 export PATH=$PATH:$HOME/Library/Python/2.7/bin

748fffa21e1239b7580fa637c2489f3fcff1ec3f
Erick Navarro <erick@navarro.io>
2020-08-31 18:00:42 -0500

    [emacs] Add emoji support using unicode-fonts package

--- .emacs.d/straight/versions/default.el
+++ .emacs.d/straight/versions/default.el
@@ -149,6 +149,7 @@
  ("tuareg" . "c12061eb80c1487a1963af7cdae268d709a70ca9")
  ("typescript.el" . "0fc729787007b5111f3584034af0f3ef2389098f")
  ("undo-tree" . "d54ef13e3c69655e39f977b8bf75399a6d2b5e42")
+ ("unicode-fonts" . "e3942fe40b418bfb2dc4e73633e09195437fef01")
  ("use-package" . "7d925367ef0857d513d62eab4cb57b7436b9ffe9")
  ("visual-fill-column" . "772d4b25ba19f57409cd03524be0f5bfdc2e8da1")
  ("vterm" . "e63bd65eece7c5de3a534b7e2fdbe58256ec2da0")

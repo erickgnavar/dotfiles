9db93262285e107ad284b678f7b6ee3e89e1c14f
Erick Navarro <erick@navarro.io>
2020-10-24 17:54:08 -0500

    [zsh] Avoid auto-update homebrew packages

--- zsh/mac.sh
+++ zsh/mac.sh
@@ -1,6 +1,9 @@
 # postgres.app setup
 export PATH=/usr/local/bin:$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 
+# Avoid auto update homwbrew packages
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 # Setup gettext binaries
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 

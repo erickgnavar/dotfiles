147b85938cdee0f67c92c642e23d937603f24e69
Erick Navarro <erick@navarro.io>
2022-01-02 11:58:14 -0800

    [emacs] Remove helm-icons
    This package has some issues with emacs 28, when a file is open from
    dired buffer and we want to search using helm it raise an error about
    `dir-closed`
    
    This package was just for decoration anyways, maybe it can be added
    later

--- .emacs.d/straight/versions/default.el
+++ .emacs.d/straight/versions/default.el
@@ -75,7 +75,6 @@
  ("haskell-mode" . "d771202bcb04c04cdfc6c102f99b917f31216390")
  ("helm" . "06c60b663babdf64383e5dc28b99ac910e04e6c8")
  ("helm-backup" . "691fe542f38fc7c8cca409997f6a0ff5d76ad6c2")
- ("helm-icons" . "8d2f5e705c8b78a390677cf242024739c932fc95")
  ("helm-bibtex" . "d4471232be26793fbf56c0ac3690b5f537c378b9")
  ("helm-ls-git" . "48696448e52d266f5b2cb5ee1390071dab4d16e8")
  ("helm-swoop" . "1f7d3cf0d742b199e4ce13fcb8b19c977a44611e")

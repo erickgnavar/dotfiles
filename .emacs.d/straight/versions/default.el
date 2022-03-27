324d5c6ffb9a192f57dcf9705a316ae666e932af
Erick Navarro <erick@navarro.io>
2022-03-27 14:43:36 -0600

    [emacs] Update lua-mode and define a lua formatter using stylua

--- .emacs.d/straight/versions/default.el
+++ .emacs.d/straight/versions/default.el
@@ -95,7 +95,7 @@
  ("logito" . "824acb89d2cc18cb47281a4fbddd81ad244a2052")
  ("lsp-dart" . "10e309acc31648ec9bf7ffeef0fc2ad16b2a8584")
  ("lsp-mode" . "d9c8ed3a932b568614d9bbf682271cf43bb8ec73")
- ("lua-mode" . "1f596a93b3f1caadd7bba01030f8c179b029600b")
+ ("lua-mode" . "5a9bee8d5fc978dc64fcb677167417010321ba65")
  ("magit" . "f84373692767a94e555cc190fd099da4570e1fca")
  ("markdown-mode" . "9977753eebe3f5cca7ab85b18a7c719fdb0b7654")
  ("marshal.el" . "f038689cbd5b3680b80b44edd0c7a63ca3038e26")

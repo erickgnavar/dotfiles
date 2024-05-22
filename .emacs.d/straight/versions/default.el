09901bd02ad5977cd2117b89ed9c852195e70029
Erick Navarro <erick@navarro.io>
2024-05-22 07:57:29 -0600

    [emacs] Remove pinned version of tree-sitter-ispell.el
    Always install latest version

--- .emacs.d/straight/versions/default.el
+++ .emacs.d/straight/versions/default.el
@@ -142,7 +142,6 @@
  ("straight.el" . "ff63b154bef1ef8d92c141bd189001bff74f6982")
  ("terraform-mode" . "e8b57df8c2a3d3171f3768f60eb84067f553289c")
  ("transient" . "04f87b4ab3c7448a78f452cadaaf5d9e7a216d1d")
- ("tree-sitter-ispell.el" . "16ba2d190cd387a0e5349c5b97a373bfad582eee")
  ("tree-sitter-langs" . "5c18599c371af14deda2a0ff12706ffbbfe171ca")
  ("treepy.el" . "de1a2ddc3fd7152d0bfbd9e14312ff8a4864ab2b")
  ("tuareg" . "c12061eb80c1487a1963af7cdae268d709a70ca9")

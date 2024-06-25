59745dc09858b57562b726e65c67401b2db0f4c0
Erick Navarro <erick@navarro.io>
2024-06-25 10:56:11 -0600

    [zsh] Replace antigen with custom script

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -3,14 +3,23 @@ then
     export HOMEBREW_PREFIX=$(brew --prefix)
 fi
 
-source "$HOMEBREW_PREFIX/share/antigen/antigen.zsh"
+mkdir -p ~/.config/zsh
 
-# Load the oh-my-zsh's library.
-antigen use oh-my-zsh
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle mafredri/zsh-async@main
+function load_zsh_plugin() {
+  url=$1
+  loader=$2
+  plugin_dir=$(echo "$url" | awk -F'[/.]' '{print $(NF-1)}')
 
-antigen apply
+  if [ ! -d "$HOME/.config/zsh/$plugin_dir" ]; then
+    git clone --depth 1 "$url" "$HOME/.config/zsh/$plugin_dir"
+  fi
+
+  . "$HOME/.config/zsh/$plugin_dir/$loader"
+}
+
+load_zsh_plugin "https://github.com/ohmyzsh/ohmyzsh.git" "oh-my-zsh.sh"
+load_zsh_plugin "https://github.com/zsh-users/zsh-syntax-highlighting.git" "zsh-syntax-highlighting.zsh"
+load_zsh_plugin "https://github.com/mafredri/zsh-async.git" "async.zsh"
 
 # load plugin from homwbrew installation
 fpath+=("$HOMEBREW_PREFIX/opt/pure/share/zsh/site-functions")

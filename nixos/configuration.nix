6ca209d0eb9ead236cb0d22a11a4756c45db6688
Erick Navarro <erick@navarro.io>
2024-07-12 09:03:58 -0600

    [nixos] Change boot configuration
    With new installation this configuration was made, this configuration
    doesn't specify the boot disk so it is more portable to share between
    different machines

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,9 +11,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.

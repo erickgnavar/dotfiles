1db5599acb87e3f817c7d4332557e195d4987aba
Erick Navarro <erick@navarro.io>
2024-07-11 18:05:34 -0600

    [emacs] Disable menu bar on linux

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,6 +1,10 @@
 ;;; early-init.el --- early init code -*- lexical-binding: t -*-
 ;;; Commentary:
 
+;; Disable menu bar only on linux, because it will be using a tiling window manager
+(when (string-equal system-type "gnu/linux")
+  (menu-bar-mode -1))
+
 ;;; Code:
 ;; Hide vertical scroll bar
 (scroll-bar-mode -1)

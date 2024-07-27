eca4f6beb0aabf122e060001fdb994f3eed5c298
Erick Navarro <erick@navarro.io>
2024-07-26 23:53:26 -0600

    [emacs] Update envrc package
    New version has support to load environment variables when running
    async-shell-command

--- .emacs.d/straight/versions/default.el
+++ .emacs.d/straight/versions/default.el
@@ -41,7 +41,7 @@
  ("emacsql" . "64012261f65fcdd7ea137d1973ef051af1dced42")
  ("embark" . "b9f2b3b9a5b9c72cf1416097b9941c4f275dae94")
  ("emmet-mode" . "0d03a59201383f37632f718012bd835222d11a8d")
- ("envrc" . "4730b31ff1479b6d822ccc7517251dcb52de45b3")
+ ("envrc" . "532ecdab48d073963ba5b581c06f3c0b83db05a6")
  ("esup" . "c9c95e245068d15d8e2732098af9a5d2bc8ec931")
  ("evil" . "0b134d3bbf73c93f5fed2bb89a35f408a9d627ed")
  ("evil-commentary" . "c5945f28ce47644c828aac1f5f6ec335478d17fb")

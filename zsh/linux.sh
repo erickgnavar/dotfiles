2a2105910040199fcd3d2935dac0d95186ceb544
Erick Navarro <erick@navarro.io>
2024-07-31 22:22:16 -0600

    [zsh] Fix copy and paste aliases to work with Wayland as well

--- zsh/linux.sh
+++ zsh/linux.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
+
 # Alias to use xclip like macOS
-alias pbcopy="xclip -selection clipboard"
-alias pbpaste="xclip -selection clipboard -o"
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+  alias pbcopy="wl-copy"
+  alias pbpaste="wl-paste"
+else
+  alias pbcopy="xclip -selection clipboard"
+  alias pbpaste="xclip -selection clipboard -o"
+fi

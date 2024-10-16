9432115a5245e0079ca3c7a3d9bd1672528f56f9
Erick Navarro <erick@navarro.io>
2024-10-16 10:55:35 -0600

    [nixos] Add flakes support

---
+++ nixos/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}

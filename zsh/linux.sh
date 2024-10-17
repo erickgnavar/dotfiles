a4e7abb7b6af205ccce9cef7db380dc2a12e5c54
Erick Navarro <erick@navarro.io>
2024-10-17 10:14:28 -0600

    [zsh] Add command to run nixos-rebuild using flakes

--- zsh/linux.sh
+++ zsh/linux.sh
@@ -8,3 +8,11 @@ else
   alias pbcopy="xclip -selection clipboard"
   alias pbpaste="xclip -selection clipboard -o"
 fi
+
+function nixos_run_install {
+  cd ~/dotfiles/nixos/ || exit 1
+  # we need to use --impure because we're referencing absolute path
+  # for hardware-configuration.nix file
+  sudo nixos-rebuild --impure switch --flake .
+  cd - || exit 1
+}

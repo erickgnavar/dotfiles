841a6dcaddcea5ca78fde96040e73ae87892000a
Erick Navarro <erick@navarro.io>
2024-10-23 13:46:11 -0600

    Enable homebrew and add some casks

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -37,6 +37,11 @@
           shfmt
         ];
 
+        homebrew = {
+          enable = true;
+          casks = [ "shortcat" "hammerspoon" ];
+        };
+
         # Auto upgrade nix package and the daemon service.
         services.nix-daemon.enable = true;
         # nix.package = pkgs.nix;

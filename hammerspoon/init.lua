75a83a65047cc021217178173481ba664b65dcb1
Erick Navarro <erick@navarro.io>
2025-01-26 14:51:48 -0600

    [hammerspoon] Add shortcuts to open more common applications

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -11,3 +11,17 @@ end)
 
 -- windows management config
 require "windows"
+
+-- shortcuts to open more common application
+hs.hotkey.bind("alt", "1", function()
+  hs.application.open "Alacritty.app"
+end)
+
+hs.hotkey.bind("alt", "2", function()
+  local defaultBrowser = hs.urlevent.getDefaultHandler "http"
+  hs.application.launchOrFocusByBundleID(defaultBrowser)
+end)
+
+hs.hotkey.bind("alt", "3", function()
+  hs.application.open "Emacs.app"
+end)

2ad61ce4f00889bbdd7f690e6d4c44952ec6cad8
Erick Navarro <erick@navarro.io>
2025-11-04 09:36:39 -0600

    [nix-darwin] Change reference to nix-darwin org and update flakes

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 

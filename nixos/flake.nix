4a8de6a094fab63da4740d2169268bfa487f3c41
Erick Navarro <erick@navarro.io>
2026-07-29 23:32:15 -0600

    [nixos] switch to unstable channel

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }: {
